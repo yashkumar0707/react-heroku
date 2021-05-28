04f1b58b5dd44dbb6803ce8a9dec76ce48e76f8f
Yash Gupta <yashkumar0707@gmail.com>
2021-05-28 17:58:02 +0530

    trying to fetch api

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     headers: { 'Accept': 'application/vnd.heroku+json; version=3', 'Content-Type': 'application/json' },
     
 };
-  fetch('https://api.heroku.com/oauth/authorizations', requestOptions,)
+  fetch('https://id.heroku.com/oauth/authorize?client_id={cb3583e5-35de-4913-b2aa-108ac7ec62bf}&response_type=code&scope={global}&state={anti-forgery-token}')
   .then(response => response.json())
   .then(json => console.log(json))
   }
