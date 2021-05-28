c87b8eff259fcf69497392fa1f1b4ba0369ffa5a
Yash Gupta <yashkumar0707@gmail.com>
2021-05-28 18:31:39 +0530

    change in package

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     headers: { 'Accept': 'application/vnd.heroku+json; version=3', 'Content-Type': 'application/json' },
     
 };
-  fetch('https://id.heroku.com/oauth/authorize?client_id={cb3583e5-35de-4913-b2aa-108ac7ec62bf}&response_type=code&scope={global}&state={anti-forgery-token}')
+  fetch('https://id.heroku.com/oauth/authorize?client_id={cb3583e5-35de-4913-b2aa-108ac7ec62bf}&response_type=code&scope={global}')
   .then(response => response.json())
   .then(json => console.log(json))
   }
